fdd0dba1c36105a9d4d7b72004abfb6ce1327c58
adsforfamebooster <153657573+adsforfamebooster@users.noreply.github.com>
2024-07-23 08:48:46 +0530

    update bot.py

--- bot.py
+++ bot.py
@@ -44,7 +44,8 @@ class Bot(Client):
                 self.LOGGER(__name__).info("\nBot Stopped...")
                 sys.exit()
         try:
-            db_channel = await self.get_chat(chat_id=int(CHANNEL_ID))
+            await app.get_messages(CHANNEL_ID, 1)
+            db_channel = await self.get_chat(chat_id=CHANNEL_ID)
             self.db_channel = db_channel
             test = await self.send_message(chat_id = db_channel.id, text = "Test Message")
             await test.delete()
